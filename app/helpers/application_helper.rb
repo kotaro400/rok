55428cbd41a79baaf6b46d4a1b982be425589fea
kotaro <ktrst0622@gmail.com>
2020-09-30 18:09:07 +0900

    add ogp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'PoK'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def full_url(path)
+    domain = "https://p-of-k.com/"
+    "#{domain}#{path}"
+  end
 end
