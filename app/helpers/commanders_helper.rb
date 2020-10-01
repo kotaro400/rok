362aafa8f50eabc7d2161a8b9c8ac51d75dcc87b
kotaro <ktrst0622@gmail.com>
2020-10-01 17:40:46 +0900

    add icon helper

--- app/helpers/commanders_helper.rb
+++ app/helpers/commanders_helper.rb
@@ -1,2 +1,14 @@
 module CommandersHelper
+
+  def commander_icon(commander)
+    case commander&.rarity
+    when "レジェンド"
+      image_tag "commander_#{commander.id}_icon", class: "legend-icon"
+    when "エピック"
+      image_tag "commander_#{commander.id}_icon", class: "epic-icon"
+    else
+      image_tag "commander"
+    end
+  end
+
 end
