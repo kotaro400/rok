322ea2e9fe2499aee3b36f369ba55871f7c8bb9c
kotaro <ktrst0622@gmail.com>
2020-10-02 15:39:47 +0900

    add team edit

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -19,15 +19,7 @@ class TeamsController < ApplicationController
       redirect_to new_team_path, alert: unit_ids.errors.full_messages.first
       return
     end
-    @team = current_user.teams.build(
-      title: params[:team][:title],
-      details: params[:team][:details],
-      unit1_id: unit_ids[0],
-      unit2_id: unit_ids[1],
-      unit3_id: unit_ids[2],
-      unit4_id: unit_ids[3],
-      unit5_id: unit_ids[4],
-    )
+    @team = current_user.teams.build(team_params(unit_ids))
     if @team.save
       redirect_to teams_path, notice: "編成が作成されました"
     else
@@ -36,10 +28,43 @@ class TeamsController < ApplicationController
     end
   end
 
+  def edit
+    @team = Team.find(params[:id])
+    @commanders = Commander.all
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    unit_ids = Unit.create_units(params[:team])
+    unless unit_ids.instance_of?(Array)
+      redirect_to new_team_path, alert: unit_ids.errors.full_messages.first
+      return
+    end
+    if @team.update(team_params(unit_ids))
+      redirect_to team_path(@team), notice: "編成を保存しました"
+    else
+      @commanders = Commander.all
+      render "edit"
+    end
+  end
+
   def destroy
     @team = Team.find(params[:id])
     @team.destroy
     redirect_to teams_path, notice: "編成を削除しました"
+  end
+
+  private
+  def team_params(unit_ids)
+    {
+      title: params[:team][:title],
+      details: params[:team][:details],
+      unit1_id: unit_ids[0],
+      unit2_id: unit_ids[1],
+      unit3_id: unit_ids[2],
+      unit4_id: unit_ids[3],
+      unit5_id: unit_ids[4],
+    }
   end
 
 end
